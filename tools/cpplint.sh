71522f9873e9582ccf56627326411f8fb8953218
Simon Sandström <simon@nikanor.nu>
2018-04-15 15:56:02 +0200

    Remove -readability/streams from cpplint

--- tools/cpplint.sh
+++ tools/cpplint.sh
@@ -4,5 +4,5 @@ set -e
 cd "$(git rev-parse --show-toplevel)"
 
 SOURCE_FILES=$(find "gameserver/" -name "*.cc" -or -name "*.h" | grep -v "/test/\|/test_export/")
-CPPLINT_FILTER="-build/include,+build/include_order,+build/include_what_you_use,-whitespace/braces,-whitespace/newline,-readability/streams"
+CPPLINT_FILTER="-build/include,+build/include_order,+build/include_what_you_use,-whitespace/braces,-whitespace/newline"
 python "external/google-styleguide/cpplint/cpplint.py" --linelength=120 --filter="$CPPLINT_FILTER" --root=gameserver $SOURCE_FILES
