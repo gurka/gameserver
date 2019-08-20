3c3c1c525b4b9dc6f4bc5f4050381b473f9117eb
Simon Sandström <simon@nikanor.nu>
2019-08-20 22:44:50 +0200

    Debug travis/lint.sh

--- tools/travis/lint.sh
+++ tools/travis/lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -x
 
 cd "$(git rev-parse --show-toplevel)"
 
@@ -13,6 +13,6 @@ python "external/google-styleguide/cpplint/cpplint.py" --linelength=120 --filter
 # Run cppcheck
 CPPCHECK_INCLUDE_DIRS=$(find gameserver/ -type d -name 'src' -or -name 'export' | grep -v '/test/' | sed 's/^/-I/')
 CPPCHECK_IGNORE_DIRS=$(find gameserver/ -type d -name 'test' | sed 's/^/-i/')
-cppcheck --std=c++11 --enable=all --suppress="unusedFunction" --error-exitcode=2 --quiet $CPPCHECK_IGNORE_DIRS $CPPCHECK_INCLUDE_DIRS gameserver/ || STATUS=1
+cppcheck --std=c++11 --enable=all --suppress="unusedFunction" --suppress="missingIncludeSystem" --error-exitcode=2 --quiet $CPPCHECK_IGNORE_DIRS $CPPCHECK_INCLUDE_DIRS gameserver/ || STATUS=1
 
 exit $STATUS
