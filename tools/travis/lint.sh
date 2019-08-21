eb54ca4cd1f92edbcaa002d30dcf605f2cf071f6
Simon Sandström <simon@nikanor.nu>
2019-08-21 22:43:27 +0200

    Update lint.sh

--- tools/travis/lint.sh
+++ tools/travis/lint.sh
@@ -13,6 +13,6 @@ python "external/google-styleguide/cpplint/cpplint.py" --linelength=120 --filter
 # Run cppcheck but ignore exit code for now
 CPPCHECK_INCLUDE_DIRS=$(find gameserver/ -type d -name 'src' -or -name 'export' | grep -v '/test/' | sed 's/^/-I/')
 CPPCHECK_IGNORE_DIRS=$(find gameserver/ -type d -name 'test' | sed 's/^/-i/')
-cppcheck --std=c++11 --enable=all --error-exitcode=2 --quiet $CPPCHECK_IGNORE_DIRS $CPPCHECK_INCLUDE_DIRS gameserver/ || STATUS=0
+cppcheck --std=c++11 --enable=all --error-exitcode=2 --quiet $CPPCHECK_IGNORE_DIRS $CPPCHECK_INCLUDE_DIRS gameserver/
 
 exit $STATUS
