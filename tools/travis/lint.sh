1078e445e01e633023f0deb0bea165294280fa7f
Simon Sandström <simon@nikanor.nu>
2019-09-12 16:22:33 +0200

    Replace google-styleguide with cpplint

--- tools/travis/lint.sh
+++ tools/travis/lint.sh
@@ -8,7 +8,7 @@ STATUS=0
 # Run cpplint
 CPPLINT_SOURCE_FILES=$(find "gameserver/" -name "*.cc" -or -name "*.h" | grep -v "/test/\|/test_export/")
 CPPLINT_FILTER="-build/include,+build/include_order,+build/include_what_you_use,-whitespace/braces,-whitespace/newline"
-python "external/google-styleguide/cpplint/cpplint.py" --linelength=120 --filter="$CPPLINT_FILTER" --root=gameserver $CPPLINT_SOURCE_FILES || STATUS=1
+python "external/cpplint/cpplint.py" --linelength=120 --filter="$CPPLINT_FILTER" --root=gameserver $CPPLINT_SOURCE_FILES || STATUS=1
 
 # Run cppcheck but ignore exit code for now
 CPPCHECK_INCLUDE_DIRS=$(find gameserver/ -type d -name 'src' -or -name 'export' | grep -v '/test/' | sed 's/^/-I/')
