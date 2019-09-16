a3d4c4836d08721bd45c63ea09a71c1089b84d3f
Simon Sandström <simon@nikanor.nu>
2019-09-16 20:10:06 +0200

    wsclient: own CMakeLists file

--- tools/cmake.sh
+++ tools/cmake.sh
@@ -51,7 +51,7 @@ function eclipse-fast {
 function wsclient {
   mkdir -p "$BUILD_DIR/wsclient"
   pushd "$BUILD_DIR/wsclient"
-  CC=emcc CXX=em++ cmake "$GAMESERVER_DIR" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DGAMESERVER_WSCLIENT=ON -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE
+  CC=emcc CXX=em++ cmake "$GAMESERVER_DIR/wsclient" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE
   popd
 }
 
