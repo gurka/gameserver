a49e438c025dae9c8645128cdc09b4078b29ce12
Simon Sandström <simon@nikanor.nu>
2019-09-16 21:39:12 +0200

    Update tools/cmake.sh

--- tools/cmake.sh
+++ tools/cmake.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -e
+set -xe
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 BUILD_DIR="$ROOT_DIR/build"
 GAMESERVER_DIR="$ROOT_DIR/gameserver"
 DATA_DIR="$ROOT_DIR/data"
 
+
 function release {
   mkdir -p "$BUILD_DIR/release"
   pushd "$BUILD_DIR/release"
-  cmake "$GAMESERVER_DIR" -DCMAKE_BUILD_TYPE=release
+  cmake "$GAMESERVER_DIR" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE -DCMAKE_BUILD_TYPE=release
   ln -sf "$DATA_DIR" data
   popd
 }
@@ -17,33 +18,15 @@ function release {
 function debug {
   mkdir -p "$BUILD_DIR/debug"
   pushd "$BUILD_DIR/debug"
-  cmake "$GAMESERVER_DIR" -DCMAKE_BUILD_TYPE=debug -DGAMESERVER_DEBUG_FULL=ON
+  cmake "$GAMESERVER_DIR" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE -DCMAKE_BUILD_TYPE=debug
   ln -sf "$DATA_DIR" data
   popd
 }
 
-function debug-fast {
+function debug-full {
   mkdir -p "$BUILD_DIR/debug-fast"
   pushd "$BUILD_DIR/debug-fast"
-  cmake "$GAMESERVER_DIR" -DCMAKE_BUILD_TYPE=debug
-  ln -sf "$DATA_DIR" data
-  popd
-}
-
-# Same as debug, but for Eclipse CDT
-function eclipse {
-  mkdir -p "$BUILD_DIR/eclipse"
-  pushd "$BUILD_DIR/eclipse"
-  cmake "$GAMESERVER_DIR" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DGAMESERVER_DEBUG_FULL=ON -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE
-  ln -sf "$DATA_DIR" data
-  popd
-}
-
-# Same as debug-fast, but for Eclipse CDT
-function eclipse-fast {
-  mkdir -p "$BUILD_DIR/eclipse-fast"
-  pushd "$BUILD_DIR/eclipse-fast"
-  cmake "$GAMESERVER_DIR" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE
+  cmake "$GAMESERVER_DIR" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE -DCMAKE_BUILD_TYPE=debug -DGAMESERVER_DEBUG_FULL=ON
   ln -sf "$DATA_DIR" data
   popd
 }
@@ -51,7 +34,7 @@ function eclipse-fast {
 function wsclient {
   mkdir -p "$BUILD_DIR/wsclient"
   pushd "$BUILD_DIR/wsclient"
-  CC=emcc CXX=em++ cmake "$GAMESERVER_DIR/wsclient" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=debug -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE
+  CC=emcc CXX=em++ cmake "$GAMESERVER_DIR/wsclient" -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_CXX_COMPILER_ARG1=-std=c++17 -DCMAKE_ECLIPSE_GENERATE_LINKED_RESOURCES=FALSE -DCMAKE_BUILD_TYPE=debug
   popd
 }
 
@@ -59,8 +42,7 @@ case $1 in
   'all')
     release
     debug
-    debug-fast
-    eclipse
+    debug-full
     wsclient
     ;;
 
@@ -72,16 +54,8 @@ case $1 in
     debug
     ;;
 
-  'debug-fast')
-    debug-fast
-    ;;
-
-  'eclipse')
-    eclipse
-    ;;
-
-  'eclipse-fast')
-    eclipse-fast
+  'debug-full')
+    debug-full
     ;;
 
   'wsclient')
@@ -89,6 +63,6 @@ case $1 in
     ;;
 
   *)
-    echo "Usage: $0 [all | release | debug | debug-fast | eclipse | eclipse-fast | wsclient]"
+    echo "Usage: $0 [all | release | debug | debug-full | wsclient]"
     ;;
 esac
