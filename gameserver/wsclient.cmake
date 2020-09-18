d3821677f6d319b206ae1589c921908629713cce
Simon Sandström <simon@nikanor.nu>
2020-09-18 12:37:19 +0200

    Prepare wsclient CMakeLists.txt

--- gameserver/wsclient.cmake
+++ gameserver/wsclient.cmake
@@ -6,67 +6,23 @@ set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -std=c++17 -O0 -g --bind -s ALLOW_MEMORY_GROWTH=1 -s EXCEPTION_DEBUG=1 -s ASSERTIONS=1 -s SAFE_HEAP=1 -s DEMANGLE_SUPPORT=1 -s USE_SDL=2")
 
+# -- Binary --
 add_subdirectory(wsclient)
 
+# -- Data files --
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/../data/data.dat" "${CMAKE_BINARY_DIR}/files/data.dat" COPYONLY)
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/../data/sprite.dat" "${CMAKE_BINARY_DIR}/files/sprite.dat" COPYONLY)
 
-# Hack to build subset of the modules that gameserver use
-
+# -- Modules --
 add_subdirectory(common)
+add_subdirectory(gameengine)
+add_subdirectory(io)
+add_subdirectory(network)
+add_subdirectory(protocol)
+add_subdirectory(utils)
+add_subdirectory(world)
 
-add_library(gameengine INTERFACE)
-target_include_directories(gameengine INTERFACE "gameengine/export")
-
-add_library(io
-  "io/export/data_loader.h"
-  "io/export/sprite_loader.h"
-  "io/src/data_loader.cc"
-  "io/src/file_reader.h"
-  "io/src/sprite_loader.cc"
-)
-target_include_directories(io PUBLIC "io/export")
-target_link_libraries(io PRIVATE
-  common
-  utils
-  rapidxml
-)
-
-add_library(network
-  "network/export/incoming_packet.h"
-  "network/export/outgoing_packet.h"
-  "network/src/incoming_packet.cc"
-  "network/src/outgoing_packet.cc"
-)
-target_include_directories(network PUBLIC "network/export")
-target_link_libraries(network PRIVATE
-  utils
-)
-
-add_library(protocol
-  "protocol/export/protocol_client.h"
-  "protocol/export/protocol_common.h"
-  "protocol/src/protocol_client.cc"
-  "protocol/src/protocol_common.cc"
-)
-target_include_directories(protocol PUBLIC "protocol/export")
-target_link_libraries(protocol PRIVATE
-  common
-  gameengine
-  network
-  world
-  utils
-)
-
-add_library(utils
-  "utils/export/logger.h"
-  "utils/src/logger.cc"
-)
-target_include_directories(utils PUBLIC "utils/export")
-
-add_library(world INTERFACE)
-target_include_directories(world INTERFACE "world/export")
-
+# -- External --
 add_library(rapidxml INTERFACE)
 target_include_directories(rapidxml SYSTEM INTERFACE "../external/rapidxml")
 
